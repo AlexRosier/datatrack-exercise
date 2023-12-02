5f2292ea5dbd3dc8d1a553d03545eb6f704415f7
arosier <3ZeE5B7whkPDhaCgX66H>
2023-12-02 12:28:25 +0100

    " vs '

--- src/integrated_exercise/pyspark/spark_clean.py
+++ src/integrated_exercise/pyspark/spark_clean.py
@@ -18,26 +18,30 @@ spark = SparkSession.builder.config(
     "com.amazonaws.auth.DefaultAWSCredentialsProviderChain",
 ).getOrCreate()
 
+categories = 'categories'
+stations = 'stations'
+timeseries = 'timeseries'
+timeseriesdata = 'timeseriesdata'
 
 def read(date: str) -> dict:
-    bucket = os.getenv("bucket")
-    df_categories = spark.read.option("multiline", "true").json(f"s3a://{bucket}/alex-data/{date}/categories")
-    df_stations = spark.read.option("multiline", "true").json(f"s3a://{bucket}/alex-data/{date}/stations")
-    df_timeseries = spark.read.option("multiline", "true").json(f"s3a://{bucket}/alex-data/{date}/timeseries")
-    df_timeseriesdata = spark.read.option("multiline", "true").json(f"s3a://{bucket}/alex-data/{date}/timeseriesdata")
+    bucket = "data-track-integrated-exercise"
+    df_categories = spark.read.json(f"s3a://{bucket}/alex-data/{date}/{categories}")
+    df_stations = spark.read.json(f"s3a://{bucket}/alex-data/{date}/{stations}")
+    df_timeseries = spark.read.json(f"s3a://{bucket}/alex-data/{date}/{timeseries}")
+    df_timeseriesdata = spark.read.json(f"s3a://{bucket}/alex-data/{date}/{timeseriesdata}")
 
-    return {'categories': df_categories, 'stations': df_stations, 'timesseries': df_timeseries,
-            'timeseriesdata': df_timeseriesdata}
+    return {categories: df_categories, stations: df_stations, timeseries: df_timeseries,
+            timeseriesdata: df_timeseriesdata}
 
 
 def transform_dataframes(dataframes: dict, date: str) -> dict:
-    df_categories = transform(dataframes.get("categories"), date)
-    df_stations = transform(dataframes.get("stations"), date)
-    df_timeseries = transform(dataframes.get("timeseries"), date)
-    df_timeseriesdata = transform_timeseries_data(dataframes.get("timeseriesdata"), date)
+    df_categories = transform(dataframes.get(categories), date)
+    df_stations = transform(dataframes.get(stations), date)
+    df_timeseries = transform(dataframes.get(timeseries), date)
+    df_timeseriesdata = transform_timeseries_data(dataframes.get(timeseriesdata), date)
 
-    return {'categories': df_categories, 'stations': df_stations, 'timesseries': df_timeseries,
-            'timeseriesdata': df_timeseriesdata}
+    return {categories: df_categories, stations: df_stations, timeseries: df_timeseries,
+            timeseriesdata: df_timeseriesdata}
 
 
 def transform(dataframe: DataFrame, date: str) -> DataFrame:
@@ -53,13 +57,13 @@ def transform_timeseries_data(df_timeseries_data: DataFrame, date: str) -> DataF
 
 def write_dataframes(dataframes: dict, date: str):
     bucket = os.getenv("bucket")
-    for key, value in dataframes:
-        value.repartition(1).write.parquet(f"s3a://{bucket}/alex-data/clean/{date}/{key}", mode="overwrite")
+    for key in dataframes.keys():
+        dataframes.get(key).write.parquet(f"s3a://{bucket}/alex-data/clean/{date}/{key}", mode="overwrite")
 
 
 def main():
-    logging.info("Entering pyspark main")
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    logging.info("Entering pyspark main")
     parser = argparse.ArgumentParser(description="Pyspark clean")
     parser.add_argument(
         "-d", "--date", dest="date", help="Date in format YYYY-mm-dd", required=True
